08fa13ab2ee499b85f7f466a49fdc80942b4f3be
dyjakowski <mdyjakowski@student.agh.edu.pl>
2025-06-09 08:46:13 +0200

    init

---
+++ src/main.js
@@ -0,0 +1,61 @@
+import { createClient } from '@supabase/supabase-js';
+import { format } from 'date-fns';
+
+const supabaseUrl = 'https://qdvphbpepwevhonxmuil.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkdnBoYnBlcHdldmhvbnhtdWlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NTMzNTksImV4cCI6MjA2MzIyOTM1OX0.6xTffFPl2eeBgLGRtBccnE2zcjqxyzNh89d088G3xAU';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+async function displayArticles() {
+  const container = document.getElementById('articles-container');
+  const sortSelect = document.getElementById('sort-select');
+  const sortValue = sortSelect ? sortSelect.value : 'created_at.desc';
+  const [column, direction] = sortValue.split('.');
+
+  const { data: article } = await supabase
+    .from('article')
+    .select('title, subtitle, author, created_at, content')
+    .order(column, { ascending: direction === 'asc' });
+
+
+  let html = '';
+  article.forEach((item, i) => {
+    html += `<div class="article">
+      <h3>Artykuł ${i + 1}</h3>
+      <p>Tytuł: ${item.title}</p>
+      <p>Podtytuł: ${item.subtitle}</p>
+      <p>Autor: ${item.author}</p>
+      <p>Data utworzenia: ${format(new Date(item.created_at), 'dd-MM-yyyy')}</p>
+      <p>Treść:${item.content}</p>
+    </div>`;
+  });
+
+  container.innerHTML = html;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  displayArticles();
+
+  const form = document.getElementById('article-form');
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const title = form.title.value;
+    const subtitle = form.subtitle.value;
+    const author = form.author.value;
+    const content = form.content.value;
+    const createdAtRaw = form.created_at.value;
+    const created_at = new Date(createdAtRaw).toISOString(); // poprawny format daty
+
+    await supabase
+      .from('article')
+      .insert([{ title, subtitle, author, content, created_at }]);
+
+    form.reset();
+    displayArticles();
+  });
+
+  const sortSelect = document.getElementById('sort-select');
+  if (sortSelect) {
+    sortSelect.addEventListener('change', displayArticles);
+  }
+});
